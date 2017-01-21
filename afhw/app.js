585d629d624b7aa54e94db329c6af460d5c9d4ea
Matthew <myzhan24@gmail.com>
2017-01-21 01:12:02 -0600

    more

--- afhw/app.js
+++ afhw/app.js
@@ -6,16 +6,47 @@
     angular.module('HelloWorldApp', [])
         .controller('HelloWorldController', function($scope) {
             $scope.name = "matthew";
-            $scope.radiuso = 1;
+            $scope.radiuso = 0.1;
             $scope.increaseRadius = function() {
-                $scope.radiuso += 0.05;
+                $scope.radiuso *= 1.10;
                 console.log('increaseRadius to ',$scope.radiuso);
             };
             $scope.expand = false;
+            $scope.animate = false;
             $scope.doExpand = function () {
                 // while ($scope.expand) {
                 //     $scope.increaseRadius();
                 // }
             };
+            $scope.moreDots = function () {
+                for (var i = 0; i < 1; i++) {
+                    pushMoreDots();
+                }
+
+            };
+
+            $scope.pos = [];
+
+            pushMoreDots();
+
+            function pushMoreDots() {
+                var temp = [];
+                for (var i = 0; i < 50; i++) {
+                    var x = getRandom()*5+1;
+                    var y = getRandom()*5+1;
+                    var z = getRandom()*5+1;
+                    temp.push({loc:''+x+" "+y+" "+z, radius:getRandom()/10});
+                }
+
+                $scope.pos = $scope.pos.concat(temp);
+            }
+
+            $scope.getRandom = function () {
+                return getRandom()/10;
+            };
+
+            function getRandom() {
+                return Math.random();
+            }
         });
 })();
