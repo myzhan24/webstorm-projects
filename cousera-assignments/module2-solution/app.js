8b578c0737bbc7cff9adc73b59671c4ed1385cce
Matthew <myzhan24@gmail.com>
2017-01-29 21:44:26 -0600

    Completed

--- cousera-assignments/module2-solution/app.js
+++ cousera-assignments/module2-solution/app.js
@@ -14,7 +14,9 @@
         var itemsToBuy = [];
         var itemsBought = [];
 
-        service.addItemsToBuy = function (itemName, quantity) {
+
+
+        service.addItemToBuy = function (itemName, quantity) {
             var item = {
                 name: itemName,
                 quantity: quantity
@@ -23,8 +25,16 @@
             itemsToBuy.push(item);
         };
 
+        service.addItemToBuy('Apples', 8);
+        service.addItemToBuy('Bananas', 23);
+        service.addItemToBuy('Cookies', 5);
+        service.addItemToBuy('Dried Apricots', 494);
+        service.addItemToBuy('Eggs', 12);
+
         service.checkOff = function (index) {
-            itemsBought.push(itemsToBuy.splice(index, 1));
+            console.log('checkOff index: '+index+', itemsToBuy: ',itemsToBuy[index]);
+            itemsBought.push(itemsToBuy.splice(index, 1)[0]);
+            console.log('itemsBought: ',itemsBought);
         };
 
         service.getItemsToBuy = function () {
@@ -41,11 +51,12 @@
     function ToBuyController(ShoppingListCheckOffService) {
         this.items = ShoppingListCheckOffService.getItemsToBuy();
 
-        ShoppingListCheckOffService.addItemsToBuy('cookies', 20);
-        ShoppingListCheckOffService.addItemsToBuy('cookies', 20);
-        ShoppingListCheckOffService.addItemsToBuy('cookies', 20);
-        ShoppingListCheckOffService.addItemsToBuy('cookies', 20);
-        ShoppingListCheckOffService.addItemsToBuy('cookies', 20);
+
+
+        this.checkOff = function (index) {
+            ShoppingListCheckOffService.checkOff(index);
+        }
+
     }
 
     AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
