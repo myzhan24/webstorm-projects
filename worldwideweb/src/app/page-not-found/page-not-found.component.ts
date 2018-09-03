6a02fd520494b410732fd25b073b34827d468142
Matthew Zhan <mzhan@metavilabs.com>
2018-09-03 17:52:28 -0500

    nerfed page not found message. updated page styling to be centered

--- worldwideweb/src/app/page-not-found/page-not-found.component.ts
+++ worldwideweb/src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-not-found.component.scss']
 })
 export class PageNotFoundComponent implements OnInit {
-
-  constructor() { }
+    constructor() { }
 
   ngOnInit() {
   }
-
 }
+
+
